851237a4f4ff17d594df90fe8efdfb87534f69b3
Casper Tyson <caspertyson99@gmail.com>
2022-09-08 21:32:13 +1200

    addded json file, and images

--- backend/database.sql
+++ backend/database.sql
@@ -5,3 +5,21 @@ CREATE TABLE users(
     userName VARCHAR(255),
     passwordHash VARCHAR(255)
 );
+
+CREATE TABLE peopleOnBail(
+    user_id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    offense VARCHAR(255),
+    longitude VARCHAR(255),
+    latitude VARCHAR(255),
+    linkToPhoto VARCHAR(255)
+);
+
+CREATE TABLE bailChecks(
+    check_id SERIAL PRIMARY KEY,
+    officerName VARCHAR(255) references users(user_id),
+    personOnBailName VARCHAR(255) references peopleOnBail(user_id),
+    checkTime BIGINT,
+    personPresent BOOLEAN,
+    comments VARCHAR(255)
+);
--- client/src/Map.js
+++ client/src/Map.js
@@ -1,22 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { useRef, useEffect, useState } from "react";
+import data1 from "./personsOnBail.json"
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 mapboxgl.accessToken =
   "pk.eyJ1IjoiYnJhZWRhbmsxODEiLCJhIjoiY2w3Zm55bG54MGM5bTNvczU3cnc3aHZ3dCJ9.PTMOm8axq4w9m_GaYfMp7Q";
-
-const data = [
-  {
-    "offense": "Robbery",
-    "name": "NPCMark",
-    coordinates: [175.2878457402682,-37.79309033810983]
-  },
-  {
-    "offense": "test1",
-    "name": "Hamilton",
-    coordinates: [175.293156,-37.793451]
-  }
-]
+const data = data1
 
 export default function App() {
   const mapContainer = useRef(null);
@@ -27,7 +16,7 @@ export default function App() {
   const [sidePanel, setSidePanel] = useState(false)
   const [offense, setOffense] = useState('')
   const [name, setName] = useState('')
-
+  const [mugshot, setMugshot] = useState('')
   
 
   function closeNav() {
@@ -42,11 +31,14 @@ export default function App() {
     });
     //Create default marker
     data.map((data) => {
+      console.log(data)
+
       const marker = new mapboxgl.Marker().setLngLat(data.coordinates).addTo(map)
       marker.getElement().addEventListener('click', () => {
         setSidePanel(true) // show side panel
         setName(data.name)
         setOffense(data.offense)
+        setMugshot(data.photoLink)
         document.getElementById("mySidepanel").style.width = "400px";
       })
     });
@@ -57,12 +49,12 @@ export default function App() {
       <div ref={mapContainer} className="map-container"/>
       {
         sidePanel ? 
-        <div id="mySidepanel" class="sidepanel">
-          <a href="javascript:void(0)" class="closebtn" onClick={closeNav}>&times;</a>
+        <div id="mySidepanel" className="sidepanel">
+          <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>&times;</a>
           <a id="title" href="#">Persn On Bail</a>
           <a href="#">Name: {name}</a>
           <a href="#">Offense: {offense}</a>
-
+          {/* <img src={require({mugshot})} alt="mugshot"/> */}
         </div>
         :
         null
